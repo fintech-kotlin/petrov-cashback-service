ad1c8b436c3c58f36b7f05dbd6d5233507ad8d34
Aleksey <50652636+al3ks3y@users.noreply.github.com>
2020-02-22 13:42:04 +0300

    Added card number masking function Added notification message creator

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,19 @@
 package ru.tinkoff.fintech.service.notification
 
+import java.lang.Exception
+
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var result:MutableList<Char> = cardNumber.toMutableList()
+
+        if(end-start<0)throw Exception("Start index cannot be greater than end index")
+        if(result.size>=end&&start>=0){
+            for(i in start until end){
+                result[i] = '#'
+            }
+        }
+        else if(result.size>0) for (i in start..15) result[i] = '#'
+        return result.toString().replace(", ","").replace("[","").replace("]","")
     }
 }
