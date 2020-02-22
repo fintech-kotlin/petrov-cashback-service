38b7f8153089cd3c57e87447664055e156aeee22
Aleksey <50652636+al3ks3y@users.noreply.github.com>
2020-02-22 13:42:04 +0300

    Added card number masking function Added notification message creator

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -7,6 +7,12 @@ class NotificationMessageGeneratorImpl(
 ) : NotificationMessageGenerator {
 
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var result:StringBuilder=StringBuilder("Уважаемый, ")
+        result.append(notificationMessageInfo.name +"!\n")
+        result.append("Спешим Вам сообщить, что на карту "+cardNumberMasker.mask(notificationMessageInfo.cardNumber)+ "\n")
+        result.append("начислен cashback в размере " + notificationMessageInfo.cashback + "\n")
+        result.append("за категорию "+ notificationMessageInfo.category + ".\n")
+        result.append("Спасибо за покупку "+notificationMessageInfo.transactionDate)
+        return result.toString()
     }
 }
