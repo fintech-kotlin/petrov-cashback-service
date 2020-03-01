8bb67dc2d9b92ed7b013e2de0e59c62c6233f4eb
Aleksey <50652636+al3ks3y@users.noreply.github.com>
2020-03-01 18:53:29 +0300

    Сделал работу над ошибками

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -5,15 +5,10 @@ import java.lang.Exception
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        var result:MutableList<Char> = cardNumber.toMutableList()
+        val result = cardNumber.toMutableList()
 
         if(end-start<0)throw Exception("Start index cannot be greater than end index")
-        if(result.size>=end&&start>=0){
-            for(i in start until end){
-                result[i] = '#'
-            }
-        }
-        else if(result.size>0) for (i in start..15) result[i] = '#'
-        return result.toString().replace(", ","").replace("[","").replace("]","")
+        result.forEachIndexed{ index, _ -> if(index in start until end) result[index]=maskChar}
+        return result.toCharArray().joinToString("","","",16,"")
     }
 }
